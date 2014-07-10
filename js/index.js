c5563b48d2a642a6e4985f7c3f8632ee995f6cf7
patric025 <patrivera25@gmail.com>
2014-07-10 17:08:02 +0800

    modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -33,7 +33,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        window.location = 'http://www.philippinerealtytv.com';
+        window.location = 'http://www.philippinerealtytv.com/index.php';
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
